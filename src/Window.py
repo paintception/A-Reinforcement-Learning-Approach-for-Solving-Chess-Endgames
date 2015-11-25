40d30b951daa7b90cfbb74a1333d0ecaaff132eb
Angelo404 <e.karountzos@hotmail.com>
2015-11-25 11:12:16 +0100

    new GUI

--- src/Window.py
+++ src/Window.py
@@ -2,42 +2,47 @@ import thread,time
 from Tkinter import *
 from Tkinter import Image
 from Cheesboard import ChessBoard
+import pygame
 
 class Window:
 	
-	def __init__(self,board,window):
-                self.board = board
-		self.createCanvas(window);
-            
-                self.console_str = StringVar()
-                fr = Frame(window)
-	        self.create_label(fr)	
+	def __init__(self, board):
+		self.board = board
+
+		self.BLACK = (  0,   0,   0)
+		self.WHITE = (255, 255, 255)
+		self.BROWN = (148, 37, 0)
+		self.LGREY = (168, 168, 168)
+
+		pygame.init()
+		size = [1200, 800]
+		self.screen = pygame.display.set_mode(size)
+		self.clock = pygame.time.Clock()
+
+		self.loadImages()
+		self.gameloop()
+
+		self.pygame.quit()
 
 
-        def create_label(self,window):
-            self.console = Label(textvariable=self.console_str, bg = '#00ff00',width= 80) 
-            self.console.pack(side = TOP, fill=X) 
+	def create_label(self,window):
+		self.console = Label(textvariable=self.console_str, bg = '#00ff00',width= 50) 
+		self.console.place(x=800,y=100) 
 
-	def createCanvas(self, window):
-		c = Canvas(window, width=800, height=800)
-		c.pack(side = LEFT)
-
+	def drawBoard(self,):
 		x = 25
 		y = 25
 		gap = 90
 		mod = 0
 
-		#VERTICAL LINES
-		#c.create_line(x, x, x, 100)
-
 		#CREATE BLOCKS
-		#c.create_rectangle(x, x, x+gap, x+gap, fill="blue") # x y width height
 		for i in range(8):
 			for j in range(8):
 				if j % 2 == mod:
-					c.create_rectangle(x, y, x+gap, y+gap, fill="White")
+					pygame.draw.rect(self.screen, self.LGREY, [x, y, gap, gap])
+					pass
 				else:
-					c.create_rectangle(x, y, x+gap, y+gap, fill="Brown")
+					pygame.draw.rect(self.screen, self.BROWN, [x, y, gap, gap])
 				x += gap
 			y += gap
 			x = 25
@@ -45,37 +50,48 @@ class Window:
 			else: mod = 0
 
 
-		self.loadImages(c)
-                self.draw_board(c)
+	def drawPieces(self):
+		p = self.board.get_wking()
+		self.screen.blit(self.wKingImg, ((p.row*90)+30,(p.col*90)+30))
 
-        def draw_board(self,c):
-                p = self.board.get_wking()
-		c.create_image((p.row * 90) + 25, 25+(p.col * 90), image=self.wking, anchor="nw", state=NORMAL)			
-                p = self.board.get_bking()
-		c.create_image((p.row * 90) + 25, 25+(p.col * 90), image=self.bking, anchor="nw", state=NORMAL)			
-                p = self.board.get_wrook()
-		c.create_image((p.row * 90) + 25, 25+(p.col * 90), image=self.wrook, anchor="nw", state=NORMAL)			
+		p = self.board.get_bking()
+		self.screen.blit(self.bKingImg, ((p.row*90)+30,(p.col*90)+30))	
 
-	def loadImages(self, c):
-		self.bking = PhotoImage(file="bking.gif")
-                self.wking =  PhotoImage(file="wking.gif")
-                self.wrook =  PhotoImage(file="wrook.gif")
-		self.bking = self.bking.subsample(6,6)
-		self.wking = self.wking.subsample(6,6)
-		self.wrook = self.wrook.subsample(6,6)
 
-def gameloop(window):
-    while True:
-        window.console_str.set(str(window.console_str.get())+'\n'+'asdfsdf sdf ')
-        time.sleep(0.1)
+		p = self.board.get_wrook()
+		self.screen.blit(self.wRookImg, ((p.row*90)+30,(p.col*90)+30))
+
+
+
+	def loadImages(self):
+		scaleFactor = 75
+
+		tmpImg = pygame.image.load("bking.gif")
+		self.bKingImg = pygame.transform.scale(tmpImg, (scaleFactor,scaleFactor))
+
+		tmpImg = pygame.image.load("wking.gif")
+		self.wKingImg = pygame.transform.scale(tmpImg, (scaleFactor,scaleFactor))
+
+		tmpImg = pygame.image.load("wrook.gif")
+		self.wRookImg = pygame.transform.scale(tmpImg, (scaleFactor,scaleFactor))
+
+	def gameloop(self):
+	    while True:
+			for event in pygame.event.get(): # User did something
+				if event.type == pygame.QUIT: # If user clicked close
+					sys.exit()
+
+			self.clock.tick(1)
+
+			self.screen.fill(self.WHITE)
+			self.drawBoard()
+			self.drawPieces()
+
+			pygame.display.flip()
+
+			#self.samlpeMove()
 
 if __name__ == '__main__':
-        board = ChessBoard.get_random_chessboard() 
-        window = Tk()
-        window.title("we are awesome")
-        window.minsize(width=1200, height=800)
-        window.resizable(width=False, height=False)
-        
-	w = Window(board,window)
-        thread.start_new_thread(gameloop,(w,))
-        window.mainloop()
+		board = ChessBoard.get_random_chessboard() 
+
+		w = Window(board)

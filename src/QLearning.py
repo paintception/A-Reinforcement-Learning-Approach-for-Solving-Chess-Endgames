7373377a86b473ff7ccfbc43397ba35172611286
Angelo404 <e.karountzos@hotmail.com>
2015-12-08 17:21:40 +0100

    Update QL

--- src/QLearning.py
+++ src/QLearning.py
@@ -14,28 +14,39 @@ class QLearning:
         self.all_params = list(self.R.keys())
 
     def learning(self, epochs):
+        current_state_id = random.choice(self.all_params)
         while epochs > 0:
-            current_state_id = random.choice(self.all_params)
             current_state = self.R[current_state_id]
             rnd_action_id = random.choice(list(current_state.keys()))
-            self.cal_learning_step(current_state_id, rnd_action_id)
-            epochs -= 1
+            res = self.cal_learning_step(current_state_id, rnd_action_id)
+            if res is None:
+                epochs -= 1
+                current_state_id = random.choice(self.all_params)
+            else:
+                current_state_id = res
 
     def cal_learning_step(self, state, action):
         mx = 0
-        for action in self.R[state]:
+        non_zero = False
+        for action in self.R[action]:
+            if action[1] != 0:
+                non_zero = True
             if action[0] > mx:
                 mx = action[0]
 
-        r = self.R[state][action][0]
-        self.R[state][action] = (r, mx*self.gamma)
+        if non_zero:
+            r = self.R[state][action][0]
+            self.R[state][action] = (r, mx*self.gamma)
+            return None
+        else:
+            return action
         
 if __name__ == '__main__':
 
     bp = BoardPossitionParams()
     q = QLearning(bp)
     print("start")
-    q.learning(10)
+    q.learning(1000000000000)
     print("save")
-    q.params.save(q.all_params, "burger10.bson")
+    q.params.save(q.all_params, "burger5000.bson")
 

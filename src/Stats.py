87e0d2d9e3889756d35607fbef199bb1cea80fd6
paintception <matthia.sabatelli@gmail.com>
2016-01-07 13:59:57 +0100

    small fixes

--- src/Stats.py
+++ src/Stats.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 
 
-
-    epochs = [ x for x in range(100,1000,100)]
+    epochs = [1000000] 
+    #epochs = [ x for x in range(100,1000,100)]
 
     games_to_play = 10000
 
@@ -27,14 +27,14 @@ if __name__ == '__main__':
 
         # CHANGE THIS WITH GAMMA FOR YARUSLAV IS 0.3 0.5,
         i= 0
-        for gamma in range(0.1,0.3,0.1):
-            fp = base_memory.split('.')[0] + '_trained_' + str(epoch) + '_' + str(int(gamma*10)) + '.bson'
+        for gamma,i in enumerate(range(1,3,1)):
+            fp = base_memory.split('.')[0] + '_trained_' + str(epoch) + '_' + str(int(gamma/10)) + '.bson'
 
             #Check if is trained
             if not os.path.isfile(fp):
                 print ('[Info] The agent is training for',epoch,'epochs')
                 bp = BoardPossitionParams()
-                q = QLearning(bp, gamma, epochs, base_memory)
+                q = QLearning(bp, gamma,0.8, epoch, base_memory)
                 q.learning()
                 q.save()
                 #exit(0)

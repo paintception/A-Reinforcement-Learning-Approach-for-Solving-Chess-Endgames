611de8d39d84db2d653622910320b3ac56903c88
Zacharias Georgiou <zacharias33@gmail.com>
2016-01-15 17:42:16 +0100

    More results needed..

--- src/plots.py
+++ src/plots.py
@@ -30,64 +30,131 @@ if __name__ == '__main__':
         ]
 
     data3 = [
-     [0.3, 1000000, 0.088, 6.81377151799687],
-     [0.3, 1500000, 0.11, 5.251572327044025],
-     [0.3, 2000000, 0.18, 9.21380846325167],
-     [0.3, 2500000, 0.26, 8.87248322147651],
-     [0.3, 3000000, 0.36, 7.246026261230131],
-     [0.3, 3500000, 0.48, 8.206806282722512],
-     [0.3, 4000000, 0.56, 7.850261780104712],
-     [0.3, 4500000, 0.65, 8.333466933867735],
-     [0.3, 5000000, 0.74, 8.333466933867735],
+         [0.3, 1000000 ,0.0882],
+         [0.3, 1500000 ,0.1127],
+         [0.3, 2000000 ,0.1887],
+         [0.3, 2500000 ,0.2692],
+         [0.3, 3000000 ,0.3656],
+         [0.3, 3500000 ,0.4885],
+         [0.3, 4000000 ,0.5655],
+         [0.3, 4500000 ,0.6592],
+         [0.3, 5000000 ,0.7458],
+         [0.3, 5500000 ,0.8271],
+         [0.3, 6000000 ,0.8736],
+         [0.3, 6500000 ,0.9057],
+         [0.3, 7000000 ,0.9258],
+         [0.3, 7500000 ,0.9273],
+
 
     ]
 
 
     data4 = [
-     [0.4, 1000000, 0.10, 6.01878612716763],
-     [0.4, 1500000, 0.18, 5.891361256544503],
-     [0.4, 2000000, 0.29, 8.385239852398524],
-     [0.4, 2500000, 0.43, 8.510227272727272],
-     [0.4, 3000000, 0.58, 7.789815277084373],
-     [0.4, 3500000, 0.69, 8.645660785019805],
-     [0.4, 4000000, 0.79, 8.810694881326851],
-     [0.4, 4500000, 0.87, 8.888997794658172],
-     [0.4, 5000000, 0.91, 8.888997794658172],
+         [0.4, 1000000 ,0.1039],
+         [0.4, 1500000 ,0.1823],
+         [0.4, 2000000 ,0.2962],
+         [0.4, 2500000 ,0.4316],
+         [0.4, 3000000 ,0.5895],
+         [0.4, 3500000 ,0.6913],
+         [0.4, 4000000 ,0.7963],
+         [0.4, 4500000 ,0.8714],
+         [0.4, 5000000 ,0.9119],
+         [0.4, 5500000 ,0.9264],
+         [0.4, 6000000 ,0.9342],
+         [0.4, 6500000 ,0.9359],
+         [0.4, 7000000 ,0.9389],
+         [0.4, 7500000 ,0.9356],
+
+    ]
+
+
+    data5 = [
+         [0.5, 1000000 ,0.1481],
+         [0.5, 1500000 ,0.2605],
+         [0.5, 2000000 ,0.4538],
+         [0.5, 2500000 ,0.6731],
+         [0.5, 3000000 ,0.792],
+         [0.5, 3500000 ,0.8806],
+         [0.5, 4000000 ,0.9227],
+         [0.5, 4500000 ,0.9326],
+         [0.5, 5000000 ,0.9393],
+         [0.5, 5500000 ,0.9341],
+         [0.5, 6000000 ,0.9332],
+         [0.5, 6500000 ,0.9379],
+
+    ]
+    data6 = [
+         [0.6, 1000000 ,0.2016],
+         [0.6, 1500000 ,0.4637],
+         [0.6, 2000000 ,0.6858],
+         [0.6, 2500000 ,0.8068],
+         [0.6, 3000000 ,0.9078],
+         [0.6, 3500000 ,0.9328],
+         [0.6, 4000000 ,0.9332],
+         [0.6, 4500000 ,0.9373],
+         [0.6, 5000000 ,0.9337],
+         [0.6, 5500000 ,0.9328],
+         [0.6, 6000000 ,0.9338],
     ]
 
 
     data8 = [
-     [0.8, 1000000, 0.5067, 6.81377151799687],
-     [0.8, 1500000, 0.8053, 5.251572327044025],
-     [0.8, 2000000, 0.9169, 9.21380846325167],
-     [0.8, 2500000, 0.9358, 8.87248322147651],
-     [0.8, 3000000, 0.9369, 8.87248322147651],
+         [0.8, 1000000 ,0.5067],
+         [0.8, 1500000 ,0.8053],
+         [0.8, 2000000 ,0.9169],
+         [0.8, 2500000 ,0.9358],
+         [0.8, 3000000 ,0.9369],
+         [0.8, 3500000 ,0.9316],
+         [0.8, 4000000 ,0.9328],
+         [0.8, 4500000 ,0.9308],
+         [0.8, 5000000 ,0.9319],
+         [0.8, 5500000 ,0.9358],
+         [0.8, 6000000 ,0.9367],
+
     ]
 
 
     data9 = [
-     [0.9, 1000000, 0.7112, 6.01878612716763],
-     [0.9, 1500000, 0.8748, 5.891361256544503],
-     [0.9, 2000000, 0.9313, 8.385239852398524],
-     [0.9, 2500000, 0.9353, 8.510227272727272],
+         [0.9, 1000000 ,0.7112],
+         [0.9, 1500000 ,0.8748],
+         [0.9, 2000000 ,0.9313],
+         [0.9, 2500000 ,0.9353],
+         [0.9, 3000000 ,0.9308],
+         [0.9, 3500000 ,0.9334],
+         [0.9, 4000000 ,0.9359],
+         [0.9, 4500000 ,0.9358],
+         [0.9, 5000000 ,0.938],
+         [0.9, 5500000 ,0.9362],
+         [0.9, 6000000 ,0.9331],
+
     ]
     data1 = np.array(data1)
     data2 = np.array(data2)
     data3 = np.array(data3)
     data4 = np.array(data4)
+    data5 = np.array(data5)
+    data6 = np.array(data6)
     data8 = np.array(data8)
     data9 = np.array(data9)
 
-    x = data1[:,  1]
+    x1 = data1[:, 1]
+    x2 = data2[:, 1]
+    x3 = data3[:, 1]
+    x4 = data4[:, 1]
+    x5 = data5[:, 1]
+    x6 = data6[:, 1]
     x8 = data8[:, 1]
     x9 = data9[:, 1]
 
-    y1 = data1[:,  2]
-    y2 = data2[:,  2]
-    y3 = data3[:,  2]
-    y4 = data4[:,  2]
-    y8 = data8[:,  2]
-    y9 = data9[:,  2]
+    y1 = data1[:, 2]
+    y2 = data2[:, 2]
+    y3 = data3[:, 2]
+    y4 = data4[:, 2]
+    y5 = data5[:, 2]
+    y6 = data6[:, 2]
+    y8 = data8[:, 2]
+    y9 = data9[:, 2]
+
 
 
     fig = matplotlib.pyplot.gcf()
@@ -97,17 +164,19 @@ if __name__ == '__main__':
     plt.title(r'Win Percentage over 10000 games with')
     plt.xlabel('Training Epochs')
     plt.ylabel('Win Percentage')
-    plt.plot(x, y1, 'r-',  linewidth=2,label='$\gamma=0.1$')
-    plt.plot(x, y2, 'b-',  linewidth=2,label='$\gamma=0.2$')
-    plt.plot(x, y3, 'g-',  linewidth=2,label='$\gamma=0.3$')
-    plt.plot(x, y4, 'y-',  linewidth=2,label='$\gamma=0.4$')
+    plt.plot(x1, y1, 'r-',  linewidth=2,label='$\gamma=0.1$')
+    plt.plot(x2, y2, 'b-',  linewidth=2,label='$\gamma=0.2$')
+    plt.plot(x3, y3, 'g-',  linewidth=2,label='$\gamma=0.3$')
+    plt.plot(x4, y4, 'y-',  linewidth=2,label='$\gamma=0.4$')
+    plt.plot(x5, y5, 'g-',  linewidth=2,label='$\gamma=0.5$')
+    plt.plot(x6, y6, 'y-',  linewidth=2,label='$\gamma=0.6$')
     plt.plot(x8, y8, 'm-',  linewidth=2,label='$\gamma=0.8$')
     plt.plot(x9, y9, 'c-',  linewidth=2,label='$\gamma=0.9$')
 
     legend = plt.legend(loc='lower right',  shadow=True,  fontsize='large')
     plt.locator_params(nbins=10)
 
-    plt.savefig('plots/win_percentage_g01_g02_g03_g04_g08_g09.png')
+    plt.savefig('plots/win_percentage_g01_g02_g03_g04_g05_g06_g08_g09.png')
 
 
     plt.clf()
@@ -115,7 +184,7 @@ if __name__ == '__main__':
     plt.title(r'Win Percentage over 10000 games with \textbf{$ \gamma = 0.1$}')
     plt.xlabel('Training Epochs')
     plt.ylabel('Win Percentage')
-    plt.plot(x,  y1,  'r-',  linewidth=2)
+    plt.plot(x1,  y1,  'r-',  linewidth=2)
     plt.savefig('plots/win_percentage_g01.png')
 
     plt.clf()
@@ -123,7 +192,7 @@ if __name__ == '__main__':
     plt.title(r'Win Percentage over 10000 games with \textbf{$ \gamma = 0.2$}')
     plt.xlabel('Training Epochs')
     plt.ylabel('Win Percentage')
-    plt.plot(x,  y2,  'r-',  linewidth=2)
+    plt.plot(x2,  y2,  'r-',  linewidth=2)
     plt.savefig('plots/win_percentage_g02.png')
 
 
@@ -132,25 +201,31 @@ if __name__ == '__main__':
     plt.title(r'Win Percentage over 10000 games with \textbf{$ \gamma = 0.3$}')
     plt.xlabel('Training Epochs')
     plt.ylabel('Win Percentage')
-    plt.plot(x,  y3,  'r-',  linewidth=2)
-
-    ## Change this
+    plt.plot(x3,  y3,  'r-',  linewidth=2)
     plt.savefig('plots/win_percentage_g03.png')
 
     plt.clf()
-    ## Change this
     plt.title(r'Win Percentage over 10000 games with \textbf{$ \gamma = 0.4$}')
     plt.xlabel('Training Epochs')
     plt.ylabel('Win Percentage')
-    plt.plot(x, y4, 'r-',  linewidth=2)
-
-    #legend = plt.legend(loc='lower right',  shadow=True,  fontsize='large')
-    #plt.locator_params(nbins=10)
-
+    plt.plot(x4, y4, 'r-',  linewidth=2)
     plt.savefig('plots/win_percentage_g04.png')
 
     plt.clf()
-    ## Change this
+    plt.title(r'Win Percentage over 10000 games with \textbf{$ \gamma = 0.5$}')
+    plt.xlabel('Training Epochs')
+    plt.ylabel('Win Percentage')
+    plt.plot(x5,  y5,  'r-',  linewidth=2)
+    plt.savefig('plots/win_percentage_g05.png')
+
+    plt.clf()
+    plt.title(r'Win Percentage over 10000 games with \textbf{$ \gamma = 0.6$}')
+    plt.xlabel('Training Epochs')
+    plt.ylabel('Win Percentage')
+    plt.plot(x6,  y6,  'r-',  linewidth=2)
+    plt.savefig('plots/win_percentage_g06.png')
+
+    plt.clf()
     plt.title(r'Win Percentage over 10000 games with \textbf{$ \gamma = 0.8$}')
     plt.xlabel('Training Epochs')
     plt.ylabel('Win Percentage')
@@ -158,9 +233,9 @@ if __name__ == '__main__':
     plt.savefig('plots/win_percentage_g08.png')
 
     plt.clf()
-    ## Change this
     plt.title(r'Win Percentage over 10000 games with \textbf{$ \gamma = 0.9$}')
     plt.xlabel('Training Epochs')
     plt.ylabel('Win Percentage')
     plt.plot(x9,  y9,  'r-',  linewidth=2)
     plt.savefig('plots/win_percentage_g09.png')
+

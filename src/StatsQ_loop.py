7caad7cd0abf1fcfe87e253e28305f8fdc0f271e
Yaroslav Shkarupa <yaroslav@shkarupa.name>
2016-02-04 14:23:38 +0100

    rounds

--- src/StatsQ_loop.py
+++ src/StatsQ_loop.py
@@ -6,7 +6,7 @@ from BaseParams import BoardPossitionParams
 
 if __name__ == '__main__':
     
-    games_to_play = 1000
+    games_to_play = 10000
 
     base_memory = 'res/memory1-0.bson'
     gamma = 0.99
@@ -15,17 +15,17 @@ if __name__ == '__main__':
     epoch_min = 500000
     epoch_max = 5000000
 
-    eps_min = 1.0
-    eps_max = 1.0
+    eps_min = 0.9
+    eps_max = 0.9
     
     while eps_min <= eps_max:
 
         while epoch_min <= epoch_max:
 
-            bp = BoardPossitionParams()
-            q = QLearning(bp, gamma=gamma, learning_rate=learning_rate, epochs=epoch_min, eps=eps_min, name=base_memory)
-            q.learning()
-            q.save()
+            # bp = BoardPossitionParams()
+            # q = QLearning(bp, gamma=gamma, learning_rate=learning_rate, epochs=epoch_min, eps=eps_min, name=base_memory)
+            # q.learning()
+            # q.save()
 
             fp = base_memory.split('.')[0] + '_Q_trained_ep' + str(epoch_min) + '_g' + str(int(gamma * 100)) + \
                    '_l' + str(int(learning_rate * 10)) + '_e' + str(int(eps_min * 100)) + '.bson'
@@ -34,7 +34,7 @@ if __name__ == '__main__':
             wins, rounds = play.play_stats(games_to_play)
             
             with open(("res/eps_"+str(int(eps_min * 100))), 'a') as outfile:
-                outfile.write(str(epoch_min)+"-"+str(wins)+"\n")
+                outfile.write(str(epoch_min)+"-"+str(wins)+"-"+str(rounds)+"\n")
             
             print('Win perc:', wins,'Average Rounds:', rounds)
             
